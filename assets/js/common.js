428d4813dcaf2cbd8dc967072bc37884d82cb21f
nancy-dalal16 <nancydalal16@gmail.com>
2025-11-24 13:03:15 +0530

    updated

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-
 // On Scroll Animation Intialization
 AOS.init();
 
@@ -13,15 +12,19 @@ function includeHTML() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("include");
           includeHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
@@ -32,4 +35,8 @@ function includeHTML() {
 
 includeHTML();
 
-
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  loader.classList.add("fade-out");
+  setTimeout(() => loader.remove(), 500);
+});
